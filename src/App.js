cb46f48bb7d1748e7671dff8d719c4213066855e
ParedesLuis <32340615+ParedesLuis@users.noreply.github.com>
2021-01-07 00:57:08 -0400

    feature: add login with logout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import {
+  AmplifyAuthenticator,
+  AmplifySignOut,
+  AmplifySignUp,
+  AmplifySignIn,
+} from "@aws-amplify/ui-react";
+
+import { AuthState, onAuthUIStateChange } from "@aws-amplify/ui-components";
+
+const form = (
+  <div>
+    <AmplifyAuthenticator usernameAlias="email">
+      <AmplifySignUp
+        slot="sign-up"
+        usernameAlias="email"
+        formFields={[
+          {
+            type: "email",
+            label: "Email",
+            placeholder: "Email",
+            required: true,
+          },
+          {
+            type: "password",
+            label: "Password",
+            placeholder: "Password",
+            required: true,
+          },
+        ]}
+      />
+      <AmplifySignIn slot="sign-in" usernameAlias="email" />
+    </AmplifyAuthenticator>
+  </div>
+);
 
 function App() {
-  return (
+  const [user, setUser] = React.useState({});
+  const [authState, setAuthState] = React.useState();
+
+  React.useEffect(() => {
+    onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AmplifySignOut />
     </div>
+  ) : (
+    form
   );
 }
 
